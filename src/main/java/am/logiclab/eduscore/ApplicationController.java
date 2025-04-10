ce3903ca244fa6fe15ebb80cc27ab64763668ae4
Raf <raf.gasparyan.17@mail.ru>
2025-04-10 22:02:26 +0400

    some changes and Security Config

--- src/main/java/am/logiclab/eduscore/ApplicationController.java
+++ src/main/java/am/logiclab/eduscore/ApplicationController.java
@@ -1,4 +1,18 @@
 package am.logiclab.eduscore;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApplicationController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/start")
+    public String test() {
+        return "start";
+    }
 }
