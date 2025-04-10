a974e902265afdbd43c0c106be90611ff66f1f5d
Raf <raf.gasparyan.17@mail.ru>
2025-04-10 21:29:07 +0400

    Initial commit

---
+++ src/main/java/am/logiclab/eduscore/EduScoreApplication.java
@@ -0,0 +1,13 @@
+package am.logiclab.eduscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduScoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/logiclab/eduscore/EduScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package am.logiclab.eduscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
